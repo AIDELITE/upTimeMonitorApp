4499051f0d122775bb87a48bea593aa542396733
Aideed <mwangaaideed@gmail.com>
2024-04-25 14:16:37 +0300

    starting the server and setting the port number

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 /*
-* primary file for the API
-*
+* Primary file for the API
+* Author: AIDEED MWANGA
+* Date: 24 - 04 - 2024
 */
 //dependencies
 
 const http = require('http');
+const url = require('url');
 
 //the server should respond to all requests with a string
 const server = http.createServer(function(req,res){
-    res.end("Hello there");
+    res.end("<h1>Hello there</h1>");
 })
 
 //start the server and have it listened to a port :3000
 
-server.listen(3000, function(req,res){
-
+server.listen(3000, function(){
+    console.log("The server is listening to port 3000 now");
 })
